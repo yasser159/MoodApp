cf2f69040d752578ef50a2f86f87958063c3cb29
Yasser Hajlaoui <yasser159@gmail.com>
2024-12-08 14:53:59 -0800

    Initial Commit

---
+++ MoodApp/MoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodAppApp.swift
+//  MoodApp
+//
+//  Created by Yasser Hajlaoui on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct MoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
